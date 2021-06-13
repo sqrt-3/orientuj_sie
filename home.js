1b499958b8f0df82f70bcde23f003b03353f1151
Paweł Dymon <dymonpawel@gmail.com>
2021-06-13 21:15:50 +0200

    block

--- home.js
+++ home.js
@@ -1,9 +1,8 @@
 function hamburgerMenu() {
-    const navMenu = document.getElementById("myLinks");
+    const navMenu = document.getElementById("myHamburgerMenu");
     if (navMenu.style.display === "flex") {
         navMenu.style.display = "none"
     } else {
         navMenu.style.display = "flex"
-
     }
 }
